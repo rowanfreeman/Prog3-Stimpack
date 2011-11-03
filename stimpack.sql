162e5b5d6e05fb3bd488afac31f5d7bad387e6a1
Rowan Freeman <primate@iinet.net.au>
2011-11-03 00:12:55 -0700

    Uploaded stimpack.sql file

---
+++ stimpack.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE IF NOT EXISTS stimpack;
+
+USE stimpack;
+
+CREATE TABLE student
+(
+student_id int NOT NULL AUTO_INCREMENT,
+username varchar(30),
+password varchar(30),
+firstname varchar(30),
+lastname varchar(30),
+phone int UNSIGNED,
+email varchar(100),
+age tinyint UNSIGNED,
+PRIMARY KEY(student_id)
+);
+
+CREATE TABLE teacher
+(
+teacher_id int NOT NULL AUTO_INCREMENT,
+username varchar(30),
+password varchar(30),
+firstname varchar(30),
+lastname varchar(30),
+phone int UNSIGNED,
+email varchar(100),
+age tinyint UNSIGNED,
+PRIMARY KEY(teacher_id)
+);
+
+CREATE TABLE class
+(
+class_id int NOT NULL AUTO_INCREMENT,
+teacher_id int,
+name varchar(50),
+maxstudents tinyint UNSIGNED,
+schedule time,
+PRIMARY KEY(class_id),
+FOREIGN KEY(teacher_id) REFERENCES teacher(teacher_id)
+);
+
+CREATE TABLE student_class
+(
+class_id int NOT NULL,
+student_id int NOT NULL,
+FOREIGN KEY(class_id) REFERENCES class(class_id),
+FOREIGN KEY(student_id) REFERENCES student(student_id)
+);
