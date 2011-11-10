fa14879395cf62df84f6587853bc71b1030a9acc
Rowan Freeman <primate@iinet.net.au>
2011-11-10 15:48:25 +1100

    Implemented edit form for student details

--- Stimpack/Stimpack-war/src/java/beans/StudentDetails.java
+++ Stimpack/Stimpack-war/src/java/beans/StudentDetails.java
@@ -10,7 +10,6 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext;
 
 /**
  *
@@ -25,8 +24,28 @@ public class StudentDetails {
     private int studentId;
     @ManagedProperty(value = "#{userManager}")
     private UserManager userManager;
+    @ManagedProperty(value="#{param.view}")
+    private int view;
+    @ManagedProperty(value="#{param.edit}")
+    private int edit;
     /** Creates a new instance of StudentDetails */
     public StudentDetails() {}
+
+    public int getEdit() {
+        return edit;
+    }
+
+    public void setEdit(int edit) {
+        this.edit = edit;
+    }
+
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
 
     public UserManager getUserManager() {
         return userManager;
@@ -37,10 +56,10 @@ public class StudentDetails {
     }
 
     public int getStudentId() {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        try {
-            this.studentId = Integer.parseInt((String)facesContext.getExternalContext().getRequestParameterMap().get("view"));
-        } catch (Exception e) {}
+        if (view != 0)
+            this.studentId = view;
+        if (edit != 0)
+            this.studentId = edit;
         if (userManager.isStudent())
             this.studentId = userManager.getStudent().getStudentId();
         return this.studentId;
