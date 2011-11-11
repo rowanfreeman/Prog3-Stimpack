36edb2ca3362f581b4897b65192e8dc0cf0a4f94
Joshua McLean <jayshoo@gmail.com>
2011-11-12 01:53:34 +1100

    Implemented enrol/unenrol

--- Stimpack/Stimpack-war/src/java/servlets/EnrolServlet.java
+++ Stimpack/Stimpack-war/src/java/servlets/EnrolServlet.java
@@ -6,23 +6,27 @@ package servlets;
 
 import ejb.Student;
 import ejb.StudentFacadeLocal;
+import ejb.Subject;
+import ejb.SubjectFacadeLocal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import pojo.md5;
 
 /**
  *
  * @author Josh
  */
-public class EditStudentServlet extends HttpServlet {
+public class EnrolServlet extends HttpServlet {
 
 	@EJB
 	private StudentFacadeLocal studentFacade;
+	@EJB
+	private SubjectFacadeLocal subjectFacade;
 
 	/** 
 	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -34,23 +38,16 @@ public class EditStudentServlet extends HttpServlet {
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 					throws ServletException, IOException {
 
-		String surname, firstname, username, password, phone, email;
-		Integer studentId = 0;
-		Short age;
+		Student student;
+		Subject subject;
+		String action = "enrol";
 
 		try {
-			try {
-				studentId = Integer.parseInt(request.getParameter("student_id"));
-			} catch (Exception e) {
-			}
-
-			surname = request.getParameter("surname");
-			firstname = request.getParameter("firstname");
-			username = request.getParameter("username");
-			password = request.getParameter("password");
-			age = Short.parseShort(request.getParameter("age"));
-			phone = request.getParameter("phone");
-			email = request.getParameter("email");
+			Integer subjectId = Integer.parseInt(request.getParameter("subject_id"));
+			Integer studentId = Integer.parseInt(request.getParameter("student_id"));
+			action = request.getParameter("action");
+			student = studentFacade.find(studentId);
+			subject = subjectFacade.find(subjectId);
 		} catch (Exception e) {
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = response.getWriter();
@@ -60,27 +57,16 @@ public class EditStudentServlet extends HttpServlet {
 			return;
 		}
 
-		Student student;
-		if (studentId > 0) {
-			student = studentFacade.find(studentId);
-		} else {
-			student = new Student();
-		}
-
-		student.setLastname(surname);
-		student.setFirstname(firstname);
-		student.setUsername(username);
-		student.setPassword(md5.md5(password));
-		student.setAge(age);
-		student.setPhone(phone);
-		student.setEmail(email);
-
 		try {
-			if (studentId > 0) {
-				studentFacade.edit(student);
-			} else {
-				studentFacade.create(student);
+			Collection<Student> studentCollection = subject.getStudentCollection();
+			if (action.equals("enrol")) {
+				studentCollection.add(student);
+			} else if (action.equals("unenrol")) {
+				studentCollection.remove(student);
 			}
+
+			subject.setStudentCollection(studentCollection);
+			subjectFacade.edit(subject);
 		} catch (Exception e) {
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = response.getWriter();
@@ -90,8 +76,7 @@ public class EditStudentServlet extends HttpServlet {
 			return;
 		}
 
-		response.sendRedirect("studentDetails.xhtml?student_id="
-						+ student.getStudentId().toString());
+		response.sendRedirect("subjectList.xhtml");
 	}
 
 	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
